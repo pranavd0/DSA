a976c6d2bff4df8c2febe2459b63a40b9994b918
pranavd0 <pranavdhawale2609@gmail.com>
2026-08-26 13:25:04 +0530

    Time: 3 ms (6.29%) | Memory: 94.3 MB (71.13%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxprofit=0;
-        int min=Integer.MAX_VALUE;
-        
-        for(int i=0;i<prices.length;i++){
-            int currprice=prices[i];
-            if(currprice<min){
-                min=currprice;
-            }
-            else{
-                int profit=currprice-min;
-                maxprofit=Math.max(maxprofit,profit);
-            }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            minPrice = Math.min(minPrice, prices[i]);
+
+            int profit = prices[i] - minPrice;
+
+            maxProfit = Math.max(maxProfit, profit);
         }
-        return maxprofit;
+
+        return maxProfit;
     }
 }
