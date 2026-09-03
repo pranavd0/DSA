2b58e3447ba989a9934c017971e0b4e6cf5730f4
pranavd0 <pranavdhawale2609@gmail.com>
2026-09-03 15:11:56 +0530

    Time: 1 ms (77.99%) | Memory: 42.3 MB (66.75%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,30 +1,32 @@
-import java.util.HashSet;
-
 class Solution {
     public boolean isHappy(int n) {
-        //unhappy number makes a loop it will form again same number which has already formed
-        //hence we will use Hashset as we will need contains function to check that number already seen
-        //whenever we get 1 afterwards all the numbers are 1
-        //hence contains loop will also stop for 1
-
-        HashSet<Integer> hs = new HashSet<>();
-        //this loop will continue until we get the same number again
-        while(!hs.contains(n)) {
-            hs.add(n);
-            //next number calculation
-            int sum = 0;
-            while(n > 0) {
-                int k = n % 10;
-                sum += k * k;
-                n = n / 10;
-            }
-            n = sum;   
+        //in this question after analysisng it we can say that there will be always a cycle if the number is unhappy
+        //cycle in the sense the specific number will repeat after fixed distance
+        //if the number is happy 1 will repeat in ending
+        //hence we can use slow fast approach here for cycle detection 
+        //even if the cycle is not present 1 will repeat which will stop our loop(make fast and slow equal)
+        //to move slow and fast we will need helper function.
+        int slow=n;
+        int fast=n;
+        //initialising slow and fast to fit in looop
+        slow=nextint(slow);
+        fast=nextint(nextint(fast));
+        //Floyds cycle detection loop
+        while(slow!=fast){
+            slow=nextint(slow);
+            fast=nextint(nextint(fast));
         }
-        //as told in question if number is 1 : happy
-        if(n == 1) {
-            return true;
+        //if loop stops on 1 number is happy
+        return slow==1;        
+    }  
+    int nextint(int a){
+        int sum=0;
+        while(a>0){
+            int k=a%10;
+            sum+=k*k;
+            a=a/10;
         }
-
-        return false;
+        return sum;
+        
     }
 }
