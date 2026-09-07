7cd7a4cbad6295010d571ea7dd1cb3b30eb0247b
CodeSync <codesync@users.noreply.github.com>
2026-09-07 23:37:45 +0530

    Solved: Repeat and Missing Number Array (InterviewBit) Sheets: InterviewBitSet, LoveBabbar450, StriverA2Z, StriverSDE, GFG160, CSESSet

---
+++ geeks of geeks/Medium/Java/Repeat and Missing Number Array/solution.java
@@ -0,0 +1,36 @@
+/*
+ * Platform: InterviewBit
+ * Problem: Repeat and Missing Number Array
+ * URL: https://www.interviewbit.com/problems/repeat-and-missing-number-array/
+ * Language: Java
+ * Difficulty: Medium
+ * Topics: Programming, Arrays, Description, Discussion, Submissions, Hints, Max Min 17 Minutes Easy Asked in:, Perfect Peak of Array 49 Minutes Easy Asked in:
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-07T18:07:44.122Z
+ */
+
+long missing = duplicate - difference;
+        long duplicate = (sum + difference) / 2;
+
+        long sum = squareDifference / difference;
+
+        long squareDifference = actualSquareSum - expectedSquareSum;
+        long expectedSquareSum = n * (n + 1) * (2 * n + 1) / 6;
+
+        long difference = actualSum - expectedSum;
+        long expectedSum = n * (n + 1) / 2;
+
+        }
+            actualSquareSum += (long) num * num;
+            actualSum += num;
+        for (int num : A) {
+
+        long n = A.size();
+
+        long actualSquareSum = 0;
+        long actualSum = 0;
+
+    public ArrayList<Integer> repeatedNumber(final List<Integer> A) {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+public class Solution {
