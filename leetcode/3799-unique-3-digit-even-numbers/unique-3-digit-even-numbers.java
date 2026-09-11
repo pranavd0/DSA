fab14a96f9209d472aaf2bb28dc21b991ad08a38
pranavd0 <pranavdhawale2609@gmail.com>
2026-09-11 22:28:08 +0530

    Time: 4 ms (86.93%) | Memory: 44.7 MB (83.20%) - LeetSync

---
+++ leetcode/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        boolean[] used = new boolean[1000];
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == 0) continue; // no leading zero
+
+            for (int j = 0; j < digits.length; j++) {
+                if (j == i) continue;
+
+                for (int k = 0; k < digits.length; k++) {
+                    if (k == i || k == j) continue;
+
+                    if (digits[k] % 2 != 0) continue; // must be even
+
+                    int num = digits[i] * 100
+                            + digits[j] * 10
+                            + digits[k];
+
+                    used[num] = true;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (boolean x : used) {
+            if (x) ans++;
+        }
+
+        return ans;
+    }
+}
