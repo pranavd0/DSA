2c49746300c24b5277ad7a2b6a48f66c7e01fc4a
pranavd0 <pranavdhawale2609@gmail.com>
2026-09-17 11:27:02 +0530

    Time: 8 ms (43.71%) | Memory: 42.1 MB (52.59%) - LeetSync

---
+++ leetcode/1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fib(int n) {
+        //step 1:Define function
+        //f(n)=f(n-1)+f(n-2)
+
+        //step 2:Base Case
+        if(n<=1){
+            return n;
+        }
+
+        //step 3:inner calls
+        int fibnm1=fib(n-1);
+        int fibnm2=fib(n-2);
+
+        //step 4:execute work
+        int fn=fibnm1+fibnm2;
+
+        //step 5:retrun
+        return fn;
+        
+    }
+}
