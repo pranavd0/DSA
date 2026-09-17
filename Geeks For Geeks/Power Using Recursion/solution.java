e66e7959c2102debdc37f93cba36eaa15094b145
CodeSync <codesync@users.noreply.github.com>
2026-09-17 16:59:08 +0530

    Solved: Power Using Recursion (GeeksforGeeks) Sheets: GFG160

---
+++ Geeks For Geeks/Power Using Recursion/solution.java
@@ -0,0 +1,35 @@
+/*
+ * Platform: GeeksforGeeks
+ * Problem: Power Using Recursion
+ * URL: https://www.geeksforgeeks.org/problems/power-using-recursion/1
+ * Language: Java
+ * Difficulty: Easy
+ * Topics: Recursion
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-17T11:29:07.421Z
+ */
+
+class Solution {
+    public int recursivePower(int n, int p) {
+        //bottom up approach
+        
+        // step 1
+        //f(p)=f(p-1)*n
+        
+        //step2
+        if(p==0){
+            return 1;
+        }
+        
+        //step 3 work
+        int fnm1=recursivePower(n,p-1);
+        
+        //step 4
+        int ans=fnm1*n;
+        
+        //step5
+        return ans;
+        
+    }
+}
