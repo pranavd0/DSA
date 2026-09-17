94822a3e1ffbd8a795889fc7810c541352e393a3
CodeSync <codesync@users.noreply.github.com>
2026-09-17 17:02:33 +0530

    Update: Power Using Recursion (GeeksforGeeks) Sheets: GFG160

--- Geeks For Geeks/Power Using Recursion/solution.java
+++ Geeks For Geeks/Power Using Recursion/solution.java
@@ -7,14 +7,14 @@
  * Topics: Recursion
  * Runtime: 0.12 s
  * Memory: N/A
- * Synced: 2026-09-17T11:32:21.870Z
+ * Synced: 2026-09-17T11:32:22.673Z
  */
 
 class Solution {
     public int recursivePower(int n, int p) {
         //bottom up approach
         
-        // step 1 define relat
+        // step 1 define relation
         //f(p)=f(p-1)*n
         
         //step2 base case
